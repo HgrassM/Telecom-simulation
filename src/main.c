ef146e9e408218af01df4caad6ef3a8fbc8b8686
HgrassM <119745077+HgrassM@users.noreply.github.com>
2024-08-03 13:32:56 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,37 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <gtk/gtk.h>
 
-#include "header/nonmodular.h"
-#include "header/bytesmodule.h"
+#include "gtkmodule.c"
 
-int main() {
-	
-	//Teste 1
-	unsigned char message[] = {'t', 'e', 's', 't', 'e', '\0'};
-	unsigned char* bit_stream = convertToBitStream(message, (int)sizeof(message));
-	printf("Size of the message is : %d\n", (int)sizeof(message));
+int main(int argc, char *argv[]) {
+	GtkApplication *app;
+	int status;
 
-	for (int i=0; i<(8*((int)sizeof(message))); i++) {
-		if (i%8 != 0) {
-			printf("%d", bit_stream[i]);
-		}else{
-			printf("\n%d", bit_stream[i]);
-		}
-	}
-	
-	printf("\n");
+	app = gtk_application_new("org.gtk.nomedoapp", G_APPLICATION_FLAGS_NONE);
+	g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+	status = g_application_run(G_APPLICATION(app), argc, argv);
+	g_object_unref(app);
 
-	free(bit_stream);
-	
-	//Teste 2
-	NRZCoordinate* coordinates_from_simulation = generateNrzPolarISignal(message, (int)sizeof(message), 500, 50, false);
 
-	for (int i=0; i<8*((int)sizeof(message)); i++) {
-		printf("(%.4fs, %.1fV)\n", coordinates_from_simulation[i].time, coordinates_from_simulation[i].voltage);  
-	}
-	
-	free(coordinates_from_simulation);
-
-	return 0;
+	return status;
 }
