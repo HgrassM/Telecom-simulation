96a7e142f9db357e3325cbc38db701c3c74f0f0d
HgrassM <119745077+HgrassM@users.noreply.github.com>
2024-08-05 22:19:31 -0300

    Upload module

---
+++ src/header/gtkmodule.h
@@ -0,0 +1,25 @@
+#ifndef GTK_MODULE_H
+#define GTK_MODULE_H
+
+typedef struct {
+	GtkWidget *text_field;
+	GtkWidget *check_button[9];
+	GtkWidget *noise_spin_button;
+	GtkWidget *band_spin_button;
+}InfoWidgets;
+
+typedef struct {
+	char *message_str;
+	double noise_power;
+	double bandwidth;
+	int digital_mod_type;
+	int carrier_mod_type;
+}GraphInfo;
+
+void draw_graph(GtkDrawingArea *drawing_area, cairo_t *cr, int width, int height, gpointer user_data);
+
+gboolean sendButtonClick(GtkWidget *widget, gpointer user_data);
+
+void activate(GtkApplication *app, gpointer user_data);
+
+#endif
