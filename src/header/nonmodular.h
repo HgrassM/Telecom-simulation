4bf0cf680ce39d8b4e8fa4f48f8ef158c60d8fdb
HgrassM <119745077+HgrassM@users.noreply.github.com>
2024-08-07 11:25:07 -0300

    Updated types

--- src/header/nonmodular.h
+++ src/header/nonmodular.h
@@ -1,22 +1,21 @@
 #ifndef NON_MODULAR_H
 #define NON_MODULAR_H
 
-struct NRZCoordinate {
-	float time;
-	float voltage;
-};
+typedef struct {
+	double time;
+	double voltage;
+}NRZCoordinate;
 
-NRZCoordinate* generateNrzPolarLSignal(unsigned char message[], int message_size, float bandwidth, float axis_voltage, bool noise_status);
+NRZCoordinate* generateNrzPolarLSignal(unsigned char message[], int message_size, double bandwidth, double axis_voltage, bool noise_status);
 
-NRZCoordinate* generateNrzPolarISignal(unsigned char message[], int message_size, float bandwidth, float axis_voltage, bool noise_status);
+NRZCoordinate* generateNrzPolarISignal(unsigned char message[], int message_size, double bandwidth, double axis_voltage, bool noise_status);
 
-NRZCoordinate* generateNrzManchesterSignal(unsigned char message[], int message_size, float bandwidth, float axis_voltage, bool noise_status);
+NRZCoordinate* generateNrzManchesterSignal(unsigned char message[], int message_size, double bandwidth, double axis_voltage, bool noise_status);
 
-NRZCoordinate* generateNrzDifferencialManchesterSignal(unsigned char message[], int message_size, float bandwidth, float axis_voltage, bool noise_status);
+NRZCoordinate* generateNrzDifferencialManchesterSignal(unsigned char message[], int message_size, double bandwidth, double axis_voltage, bool noise_status);
 
-NRZCoordinate* generateNrzBipolarAMISignal(unsigned char message[], int message_size, float bandwidth, float axis_voltage, bool noise_status);
+NRZCoordinate* generateNrzBipolarAMISignal(unsigned char message[], int message_size, double bandwidth, double axis_voltage, bool noise_status);
 
-NRZCoordinate* generateNrzBipolarPseudoternarySignal(unsigned char message[], int message_size, float bandwidth, float axis_voltage, bool noise_status);
+NRZCoordinate* generateNrzBipolarPseudoternarySignal(unsigned char message[], int message_size, double bandwidth, double axis_voltage, bool noise_status);
 
 #endif
-
