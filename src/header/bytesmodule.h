01bddadc7c3423c2280e09ce2d12a18c6b1bbea9
HgrassM <119745077+HgrassM@users.noreply.github.com>
2024-08-17 21:05:29 -0300

    Added framing functions implementations

--- src/header/bytesmodule.h
+++ src/header/bytesmodule.h
@@ -7,4 +7,10 @@ unsigned char* convertToByteStream(unsigned char* message_bits, int message_bits
 
 void generateError(unsigned char* message_bits, int message_bits_size, int error_percentage);
 
+unsigned char* frameByCharacterCounting(unsigned char* raw_message, int raw_message_size);
+
+unsigned char* frameByBytesFlag(unsigned char* raw_message, int raw_message_size);
+
+unsigned char* frameByBitsFlag(unsigned char* raw_message_bits, int raw_message_bits_size);
+
 #endif
