df16a27d74726d43baf9bfcdca8360305f57b726
HgrassM <119745077+HgrassM@users.noreply.github.com>
2024-09-01 17:36:32 -0300

    Update bytesmodule.h

--- src/header/bytesmodule.h
+++ src/header/bytesmodule.h
@@ -1,32 +1,39 @@
 #ifndef BYTES_MODULE_H
 #define BYTES_MODULE_H
 
+typedef struct {
+	unsigned char* result_message;
+	int result_message_length;
+}ResultWithSize;
+
 unsigned char* convertToBitStream(unsigned char message[], int message_size);
 
-unsigned char* convertToByteStream(unsigned char* message_bits, int message_bits_size);
+ResultWithSize convertToByteStream(unsigned char* message_bits, int message_bits_size);
 
 void generateError(unsigned char* message_bits, int message_bits_size, int error_percentage);
 
-unsigned char* frameByCharacterCounting(unsigned char* raw_message, int raw_message_size);
+ResultWithSize frameByCharacterCounting(unsigned char* raw_message, int raw_message_size);
 
-unsigned char* frameByBytesFlag(unsigned char* raw_message, int raw_message_size);
+ResultWithSize frameByBytesFlag(unsigned char* raw_message, int raw_message_size);
 
-unsigned char* frameByBitsFlag(unsigned char* raw_message_bits, int raw_message_bits_size);
+ResultWithSize frameByBitsFlag(unsigned char* raw_message_bits, int raw_message_bits_size);
 
-unsigned char* createErrorCheckBitParity(unsigned char* received_data_bits, int received_data_bits_length);
+ResultWithSize createErrorCheckBitParity(unsigned char* received_data_bits, int received_data_bits_length);
 
-unsigned char* generateCrcErrorCheck(unsigned char* received_data_bits, int received_data_bits_length);
+ResultWithSize generateCrcErrorCheck(unsigned char* received_data_bits, int received_data_bits_length);
 
-unsigned char* generateHammingErrorCorrection(unsigned char* received_data_bits, int received_data_bits_length);
+ResultWithSize generateHammingErrorCorrection(unsigned char* received_data_bits, int received_data_bits_length);
 
-unsigned char* getMessageFromByteFrame(unsigned char* received_message, int message_length);
+ResultWithSize getMessageFromCharacterFrame(unsigned char* received_message, int message_length);
 
-unsigned char* getMessageFromBitFrame(unsigned char* received_message_bits, int message_length);
+ResultWithSize getMessageFromByteFrame(unsigned char* received_message, int message_length);
 
-unsigned char* verifyErrorByParity(unsigned char* received_message_bits, int message_length);
+ResultWithSize getMessageFromBitFrame(unsigned char* received_message_bits, int message_length);
 
-unsigned char* verifyErrorByCrc(unsigned char* received_message_bits, int message_length);
+ResultWithSize verifyErrorByParity(unsigned char* received_message_bits, int message_length);
 
-unsigned char* verifyErrorByHamming(unsigned char* received_message_bits, int message_size);
+ResultWithSize verifyErrorByCrc(unsigned char* received_message_bits, int message_length);
+
+ResultWithSize verifyErrorByHamming(unsigned char* received_message_bits, int message_size);
 
 #endif
