c7565eef2c0a2ba91766b54177c6e63eb861b0dd
HgrassM <119745077+HgrassM@users.noreply.github.com>
2024-09-01 17:35:28 -0300

    Linked all the modules

--- src/gtkmodule.c
+++ src/gtkmodule.c
@@ -1,15 +1,263 @@
 #include <gtk/gtk.h>
+
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdio.h>
 
+#include <pthread.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
 #include "header/nonmodular.h"
 #include "header/gtkmodule.h"
 #include "header/carriermodule.h"
+#include "header/bytesmodule.h"
 
+//Global variables
 static int digital_coord_arr_size = 0;
 double *nrz_time = NULL;
 bool isManchester = false;
+bool finishThread = false;
+bool canExit = true;
+MessageInfo thread_message_info;
+
+//Global concurrency controll variables
+pthread_mutex_t thread_mutex;
+pthread_cond_t thread_cond;
+
+//Verifies if main window has been closed
+gboolean main_window_status(GtkWidget* widget, gpointer gtk_data) {
+	InfoWidgets* data_from_gtk = (InfoWidgets*) gtk_data;
+	GtkWidget* main_thread_window = data_from_gtk->main_thread_window;
+
+	pthread_mutex_lock(&thread_mutex);
+	finishThread = true;	
+	pthread_cond_signal(&thread_cond);
+	pthread_mutex_unlock(&thread_mutex);
+
+	if (canExit) {
+		close(data_from_gtk->transmitter_socket);
+		close(data_from_gtk->receptor_socket);
+		gtk_window_close(GTK_WINDOW(data_from_gtk->receptor_window));
+		free(data_from_gtk);	
+		exit(0);
+	}
+}
+
+void* receptor_execution(void* gtk_data) {
+	//Widgets data
+	InfoWidgets* data_from_gtk = (InfoWidgets*) gtk_data;
+
+	//Receptor socket
+	int receptor_socket = data_from_gtk->receptor_socket;
+	
+	//socket to receive data
+	int returned_socket;
+
+	//Accepts transmissor request
+	returned_socket = accept(receptor_socket, NULL, NULL);
+	canExit = false;
+	
+	//Binary structs
+	ResultWithSize encoded_message_bytes;
+	ResultWithSize message_with_frames;
+	ResultWithSize raw_message_bits;
+	ResultWithSize raw_message;
+	
+	//Flag
+	bool isByte = true;
+
+	//Received encoded message
+	unsigned char encoded_message[131072];
+	int encoded_message_length = 0;	
+	
+	//Label strings
+	char label_bits_num_str[50];
+	char label_decoded_message_str[50];
+
+	//Setting label
+	GtkWidget* label_bits_num = data_from_gtk->label_bits_num;
+	GtkWidget* label_warning_1 = data_from_gtk->label_warning_1;
+	GtkWidget* label_warning_2 = data_from_gtk->label_warning_2;
+	GtkWidget* label_decoded_message = data_from_gtk->label_decoded_message;
+	GtkWidget* receptor_window = data_from_gtk->receptor_window;	
+
+	//While the main window hasn't been closed, the thread keeps running
+	pthread_mutex_lock(&thread_mutex);
+	while (true) {	
+		//Flag
+		isByte = true;
+	
+		//Wait for the message convention
+		pthread_cond_wait(&thread_cond, &thread_mutex);
+		
+		if (finishThread) {
+			break;
+		}
+
+		encoded_message_length = recv(returned_socket, &encoded_message, sizeof(encoded_message), 0);
+		
+		//Simulates error during transmission
+		generateError(encoded_message, encoded_message_length, (int) thread_message_info.error_probability);
+		
+		//Print
+		sprintf(label_bits_num_str, "Received %d bits", encoded_message_length);
+		
+		//Error detection method		
+		switch(thread_message_info.error_detection_type) {
+			case 0:
+				message_with_frames = verifyErrorByParity(encoded_message, encoded_message_length);			
+				break;
+			case 1:
+				message_with_frames = verifyErrorByCrc(encoded_message, encoded_message_length);
+				break;
+			case 2:
+				message_with_frames = verifyErrorByHamming(encoded_message, encoded_message_length);
+				break;
+		}
+		
+		//Framing method
+		if (message_with_frames.result_message != NULL) {
+			encoded_message_bytes = convertToByteStream(message_with_frames.result_message, message_with_frames.result_message_length);
+			
+			switch(thread_message_info.framing_type) {
+				case 0:
+					raw_message = getMessageFromCharacterFrame(encoded_message_bytes.result_message, encoded_message_bytes.result_message_length);	
+					break;
+				case 1:	
+					raw_message = getMessageFromByteFrame(encoded_message_bytes.result_message, encoded_message_bytes.result_message_length);
+					break;
+				case 2:
+					isByte = false;
+					raw_message_bits = getMessageFromBitFrame(message_with_frames.result_message, message_with_frames.result_message_length);
+					break;
+			}
+			
+			gtk_label_set_text(GTK_LABEL(label_warning_1), "  ");
+			free(encoded_message_bytes.result_message);
+			free(message_with_frames.result_message);	
+		}else{
+			gtk_label_set_text(GTK_LABEL(label_decoded_message), "  ");
+			gtk_label_set_text(GTK_LABEL(label_warning_1), "An unfixable error has been detected");
+		}
+		
+		if (!isByte) {
+			raw_message = convertToByteStream(raw_message_bits.result_message, raw_message_bits.result_message_length);
+			free(raw_message_bits.result_message);
+		}
+
+		if (raw_message.result_message != NULL) {
+			gtk_label_set_text(GTK_LABEL(label_warning_2), "  ");	
+			sprintf(label_decoded_message_str, "The decoded message is: %s", raw_message.result_message);
+			gtk_label_set_text(GTK_LABEL(label_decoded_message), label_decoded_message_str);
+			free(raw_message.result_message);	
+		}else{
+			gtk_label_set_text(GTK_LABEL(label_decoded_message), "  ");
+			gtk_label_set_text(GTK_LABEL(label_warning_2), "Error in flag");	
+		}
+
+		gtk_label_set_text(GTK_LABEL(label_bits_num), label_bits_num_str);			
+	}
+	pthread_mutex_unlock(&thread_mutex);
+	
+	//Closes the receptor's window
+	gtk_window_close(GTK_WINDOW(receptor_window));
+
+	//Destroying concurrency controll variables
+	pthread_mutex_destroy(&thread_mutex);
+	pthread_cond_destroy(&thread_cond);
+
+	//Closes sockets
+	close(data_from_gtk->transmitter_socket);
+	close(receptor_socket);
+	close(returned_socket);
+
+	//Frees memory
+	free(data_from_gtk);
+
+	exit(0);
+}
+
+void sock_send_message(unsigned char* raw_message, MessageInfo message_info, int transmitter_socket, GraphInfo graph_info) {
+	unsigned char* message_with_frames_in_bits = NULL;
+	unsigned char* raw_message_bits = NULL;
+	ResultWithSize message_with_error_detection;
+	ResultWithSize message_with_frames;
+
+	int raw_message_size = strlen(raw_message) + 1;
+	int raw_message_size_in_bits = raw_message_size*8;
+
+	bool isInByte = true;
+
+
+	switch (message_info.framing_type) {
+		case 0:		
+			message_with_frames = frameByCharacterCounting(raw_message, raw_message_size);
+			break;
+		case 1:			
+			message_with_frames = frameByBytesFlag(raw_message, raw_message_size);
+			break;
+		case 2:
+			isInByte = false;
+			raw_message_bits = convertToBitStream(raw_message, raw_message_size);
+			
+			message_with_frames = frameByBitsFlag(raw_message_bits, raw_message_size_in_bits);
+			free(raw_message_bits);
+			break;
+	}
+	
+	//If the framing is successfull, the message is sent
+	if (message_with_frames.result_message != NULL) {	
+		if (isInByte) {
+			message_with_frames_in_bits = convertToBitStream(message_with_frames.result_message, message_with_frames.result_message_length);
+			
+			switch (message_info.error_detection_type) {
+				case 0:
+					message_with_error_detection = createErrorCheckBitParity(message_with_frames_in_bits, message_with_frames.result_message_length*8);
+					break;
+				case 1:
+					message_with_error_detection = generateCrcErrorCheck(message_with_frames_in_bits, message_with_frames.result_message_length*8);
+					break;
+				case 2:
+					message_with_error_detection = generateHammingErrorCorrection(message_with_frames_in_bits, message_with_frames.result_message_length*8);
+					break;
+			}
+			
+			free(message_with_frames.result_message);
+		}else{	
+			switch (message_info.error_detection_type) {
+				case 0:
+					message_with_error_detection = createErrorCheckBitParity(message_with_frames.result_message, message_with_frames.result_message_length);
+					break;
+				case 1:
+					message_with_error_detection = generateCrcErrorCheck(message_with_frames.result_message, message_with_frames.result_message_length);
+					break;
+				case 2:
+					message_with_error_detection = generateHammingErrorCorrection(message_with_frames.result_message, message_with_frames.result_message_length);
+					break;
+			}
+			
+		}
+		
+	}
+	
+	//If the error detection implementation is successfull, the message is sent
+	if (message_with_error_detection.result_message != NULL) {
+		send(transmitter_socket, message_with_error_detection.result_message, message_with_error_detection.result_message_length, 0);
+	}
+	
+	//Generates graph of the physics layer signals
+	generateGraph(graph_info);
+
+	//Deallocate memory
+	free(message_with_error_detection.result_message);
+	free(raw_message);			
+
+	//Signals to receptors thread
+	pthread_cond_signal(&thread_cond);
+}
 
 void draw_graph_carrier(GtkDrawingArea *drawing_area, cairo_t *cr, int width, int height, gpointer user_data) {
 	SinCoordinate *data_to_draw = (SinCoordinate*) user_data;
@@ -380,6 +628,8 @@ void generateGraph(GraphInfo graph_info) {
 	//Carrier modulation drawing area configuration
 	gtk_drawing_area_set_draw_func(GTK_DRAWING_AREA(carrier_drawing_area), draw_graph_carrier, carrier_coordinates, NULL);
 
+	//Deallocating memory
+	free(graph_info.message_str);
 }
 
 gboolean sendButtonClick(GtkWidget *widget, gpointer user_data) {
@@ -394,6 +644,20 @@ gboolean sendButtonClick(GtkWidget *widget, gpointer user_data) {
 	int c_value = 0;
 	int error_value = 0;
 	int frame_value = 0;
+	
+	//If the tranasmitter's socket isn't connected to the receptor's socket, the connection is made
+	while (data->connection_status == -1) {
+
+		//Connecting the transmitter's socket to the receptor's socket
+		data->connection_status = connect(data->transmitter_socket, (struct sockaddr*) &data->transmitter_address, sizeof(data->transmitter_address));
+		
+		if (data->connection_status == -1) {
+			g_print("Error: %s\n", strerror(errno));
+			close(data->transmitter_socket);
+
+			data->transmitter_socket = socket(AF_INET, SOCK_STREAM, 0);	
+		}
+	}
 
 	GtkTextBuffer *buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(text));	
 	GtkTextIter start_iter;
@@ -435,11 +699,20 @@ gboolean sendButtonClick(GtkWidget *widget, gpointer user_data) {
 		frame_value
 	};
 	
-	generateGraph(graph_info);
+	//Message config
+	pthread_mutex_lock(&thread_mutex);
+	thread_message_info = message_info;
+	pthread_mutex_unlock(&thread_mutex);	
+	
+	sock_send_message(gtk_text_buffer_get_text(buffer, &start_iter, &end_iter, FALSE), message_info, data->transmitter_socket, graph_info);
+	return true;
 }
 
 //This function executes when the gtk app object emits an 'activate' signal
 void activate(GtkApplication *app, gpointer user_data) {
+	//Declaring thread instances
+	pthread_t receptor_thread;
+	
 	//Declaring window
 	GtkWidget *main_window;
 
@@ -625,9 +898,65 @@ void activate(GtkApplication *app, gpointer user_data) {
 		}else if (i>12) {
 			gtk_check_button_set_group(GTK_CHECK_BUTTON(check_box[i]), GTK_CHECK_BUTTON(check_box[12]));
 		}
+	}	
+
+	//Receptor's socket
+	int receptor_socket = -1; 
+	receptor_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+	//Address of the receptor's socket
+	struct sockaddr_in receptor_address;
+	receptor_address.sin_family = AF_INET;
+	receptor_address.sin_port = htons(8000);
+	receptor_address.sin_addr.s_addr = INADDR_ANY;
+
+	//Binding the receptor's socket to the address and listen for connections
+	if (bind(receptor_socket, (struct sockaddr*) &receptor_address, sizeof(receptor_address)) == -1) {
+		g_print("%s\n", strerror(errno));
+		close(receptor_socket);
+		exit(1);
 	}
 
-	//Binding the callback functions to the widgets	
+	if (listen(receptor_socket, 1) == -1) {
+		g_print("%s\n", strerror(errno));
+		close(receptor_socket);
+		exit(1);
+	}
+
+	//Transmitter's socket
+	int transmitter_socket = -1;
+	transmitter_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+	//Transmitter's address
+	struct sockaddr_in transmitter_address;
+	transmitter_address.sin_family = AF_INET;
+	transmitter_address.sin_port = htons(8000);
+	transmitter_address.sin_addr.s_addr = INADDR_ANY;	
+	
+	//GTK widgets for receptor's window
+	GtkWidget* receptor_window = gtk_window_new();
+	
+	GtkWidget* title_label = gtk_label_new("RECEPTOR'S CONSOLE");
+	GtkWidget* label_bits_num = gtk_label_new("");
+	GtkWidget* label_warning_1 = gtk_label_new("");
+	GtkWidget* label_warning_2 = gtk_label_new("");
+	GtkWidget* label_decoded_message = gtk_label_new("");
+
+	GtkWidget* vertical_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+
+	//Appending to the box and window
+	gtk_box_append(GTK_BOX(vertical_box), title_label);
+	gtk_box_append(GTK_BOX(vertical_box), label_bits_num);
+	gtk_box_append(GTK_BOX(vertical_box), label_warning_1);
+	gtk_box_append(GTK_BOX(vertical_box), label_warning_2);
+	gtk_box_append(GTK_BOX(vertical_box), label_decoded_message);
+	
+	gtk_window_set_deletable(GTK_WINDOW(receptor_window), FALSE);
+	gtk_window_set_title(GTK_WINDOW(receptor_window), "Receptor window");
+	gtk_window_set_default_size(GTK_WINDOW(receptor_window), 300, 300);
+	gtk_window_set_child(GTK_WINDOW(receptor_window), vertical_box);	
+
+	//Setting function's data
 	
 	InfoWidgets *data = (InfoWidgets*) malloc(sizeof(InfoWidgets));
 
@@ -640,14 +969,40 @@ void activate(GtkApplication *app, gpointer user_data) {
 	data->noise_spin_button = noise_spin_button;
 	data->band_spin_button = band_spin_button;
 	data->error_prob_spin_button = error_prob_spin_button;
+	data->main_thread_window = main_window;
+	data->receptor_socket = receptor_socket;
+	data->transmitter_socket = transmitter_socket;
+	data->connection_status = -1;
+	data->transmitter_address = transmitter_address;
+	data->label_bits_num = label_bits_num;
+	data->label_warning_1 = label_warning_1;
+	data->label_warning_2 = label_warning_2;
+	data->label_decoded_message = label_decoded_message;
+	data->receptor_window = receptor_window;	
+	
+	//Initializing concurrency controll variables
+	pthread_mutex_init(&thread_mutex, NULL);
+	pthread_cond_init(&thread_cond, NULL);
 
-	g_signal_connect(send_button, "clicked", G_CALLBACK(sendButtonClick), data); 	
+	//Creating receptror's thread routine
+	if (pthread_create(&receptor_thread, NULL, &receptor_execution, (void*) data) != 0) {
+		g_print("Error creating thread");
+		close(transmitter_socket);
+		close(receptor_socket);
+		exit(1);
+	}
+	
+	//Setting send button callback function
+	g_signal_connect(send_button, "clicked", G_CALLBACK(sendButtonClick), data);
+	g_signal_connect_after(main_window, "destroy", G_CALLBACK(main_window_status), data);
 
 	//Setting window's configuration
 	gtk_window_set_title(GTK_WINDOW(main_window), "Telecom simulation");
 	gtk_window_set_default_size(GTK_WINDOW(main_window), 300, 600);
 
 	gtk_window_set_child(GTK_WINDOW(main_window), main_vertical_container);
-
+	
+	//Display window to the user
 	gtk_window_present(GTK_WINDOW(main_window));
+	gtk_window_present(GTK_WINDOW(receptor_window));
 }
