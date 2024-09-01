4e6a907d2b27545d8e85366d14a6168b22a88d6c
HgrassM <119745077+HgrassM@users.noreply.github.com>
2024-09-01 17:37:01 -0300

    Update gtkmodule.h

--- src/header/gtkmodule.h
+++ src/header/gtkmodule.h
@@ -1,3 +1,8 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include <netinet/in.h>
+
 #ifndef GTK_MODULE_H
 #define GTK_MODULE_H
 
@@ -7,6 +12,16 @@ typedef struct {
 	GtkWidget *noise_spin_button;
 	GtkWidget *band_spin_button;
 	GtkWidget *error_prob_spin_button;
+	GtkWidget *main_thread_window;
+	GtkWidget *receptor_window;
+	GtkWidget *label_bits_num;
+	GtkWidget *label_warning_1;
+	GtkWidget *label_warning_2;
+	GtkWidget *label_decoded_message;
+	int receptor_socket;
+	int transmitter_socket;
+	int connection_status;
+	struct sockaddr_in transmitter_address;
 }InfoWidgets;
 
 typedef struct {
@@ -22,6 +37,14 @@ typedef struct {
 	int error_detection_type;
 	int framing_type;
 }MessageInfo;
+
+gboolean main_window_status(GtkWidget* widget, gpointer gtk_data);
+
+void* receptor_execution(void* gtk_data);
+
+void generateGraph(GraphInfo graph_info);
+
+void sock_send_message(unsigned char* raw_message, MessageInfo message_info, int transmitter_socket, GraphInfo graph_info);
 
 void draw_graph_digital(GtkDrawingArea *drawing_area, cairo_t *cr, int width, int height, gpointer user_data);
 
